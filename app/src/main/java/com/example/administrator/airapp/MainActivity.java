7dc4fd11db9473c42054987ba0e262d37eba542b
lsk9768 <lsk9768@nate.com>
2017-06-21 13:50:26 +0900

    MPAndroid add

--- app/src/main/java/com/example/administrator/airapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/airapp/MainActivity.java
@@ -4,91 +4,32 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+
+import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView datetime;
-    ProgressBar seoulBar;
-    TextView seoul;
-    ProgressBar busanBar;
-    TextView busan;
-    ProgressBar daeguBar;
-    TextView daegu;
-    ProgressBar incheonBar;
-    TextView incheon;
-    ProgressBar gwangjuBar;
-    TextView gwangju;
-    ProgressBar daejeonBar;
-    TextView daejeon;
-    ProgressBar ulsanBar;
-    TextView ulsan;
-    ProgressBar gyeonggiBar;
-    TextView gyeonggi;
-    ProgressBar gangwonBar;
-    TextView gangwon;
-    ProgressBar chungbukBar;
-    TextView chungbuk;
-    ProgressBar chungnamBar;
-    TextView chungnam;
-    ProgressBar jeonbukBar;
-    TextView jeonbuk;
-    ProgressBar jeonnamBar;
-    TextView jeonnam;
-    ProgressBar gyeongbukBar;
-    TextView gyeongbuk;
-    ProgressBar gyeongnamBar;
-    TextView gyeongnam;
-    ProgressBar jejuBar;
-    TextView jeju;
-    ProgressBar sejongBar;
-    TextView sejong;
+    BarChart barChart;
+    ArrayList<String> labels;
+    String dataTime;
+    ArrayList<BarEntry> entries;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        barChart = (BarChart) findViewById(R.id.barChart);
         new MyThred().execute();
-        datetime = (TextView) findViewById(R.id.datetime);
-        seoul = (TextView) findViewById(R.id.seoul);
-        seoulBar = (ProgressBar) findViewById(R.id.seoulBar);
-        busan =(TextView) findViewById(R.id.busan);
-        busanBar = (ProgressBar) findViewById(R.id.busanBar);
-        daegu =(TextView) findViewById(R.id.daegu);
-        daeguBar = (ProgressBar) findViewById(R.id.daeguBar);
-        incheon =(TextView) findViewById(R.id.incheon);
-        incheonBar = (ProgressBar) findViewById(R.id.incheonBar);
-        gwangju =(TextView) findViewById(R.id.gwangju);
-        gwangjuBar = (ProgressBar) findViewById(R.id.gwangjuBar);
-        daejeon =(TextView) findViewById(R.id.daejeon);
-        daejeonBar = (ProgressBar) findViewById(R.id.daejeonBar);
-        ulsan =(TextView) findViewById(R.id.ulsan);
-        ulsanBar = (ProgressBar) findViewById(R.id.ulsanBar);
-        gyeonggi =(TextView) findViewById(R.id.gyeonggi);
-        gyeonggiBar = (ProgressBar) findViewById(R.id.gyeonggiBar);
-        gangwon =(TextView) findViewById(R.id.gangwon);
-        gangwonBar = (ProgressBar) findViewById(R.id.gangwonBar);
-        chungbuk =(TextView) findViewById(R.id.chungbuk);
-        chungbukBar = (ProgressBar) findViewById(R.id.chungbukBar);
-        chungnam =(TextView) findViewById(R.id.chungnam);
-        chungnamBar = (ProgressBar) findViewById(R.id.chungnamBar);
-        jeonbuk =(TextView) findViewById(R.id.jeonbuk);
-        jeonbukBar = (ProgressBar) findViewById(R.id.jeonbukBar);
-        gyeongbuk =(TextView) findViewById(R.id.gyeongbuk);
-        gyeongbukBar = (ProgressBar) findViewById(R.id.gyeongbukBar);
-        gyeongnam =(TextView) findViewById(R.id.gyeongnam);
-        gyeongnamBar = (ProgressBar) findViewById(R.id.gyeongnamBar);
-        jeonnam =(TextView) findViewById(R.id.jeonnam);
-        jeonnamBar = (ProgressBar) findViewById(R.id.jeonnamBar);
-        jeju =(TextView) findViewById(R.id.jeju);
-        jejuBar = (ProgressBar) findViewById(R.id.jejuBar);
-        sejong =(TextView) findViewById(R.id.sejong);
-        sejongBar = (ProgressBar) findViewById(R.id.sejongBar);
+
     }
-    class MyThred extends AsyncTask{
+    class MyThred extends AsyncTask {
         @Override
         protected Object doInBackground(Object[] params){
             Map<String, String> map= AirService.getAir();
@@ -104,8 +45,26 @@ public class MainActivity extends AppCompatActivity {
                 sb.append(KeyName + ":" + map.get(KeyName)+"\n");
             }
             Log.d("air",sb.toString());
+            dataTime = map.get("dataTime");
+            map.remove("dataTime");
+            labels = new ArrayList<String>();
+            entries = new ArrayList<BarEntry>();
+            int index = 0;
 
+            for(String KeyName : map.keySet()) {
+                labels.add(KeyName);
+                entries.add(new BarEntry(index++,Float.parseFloat(map.get(KeyName))));
 
+            }
+            Log.d("air",labels.toString());
+
+            BarDataSet barDataSet = new BarDataSet(entries, "Korea air");
+            barDataSet.setDrawValues(false);
+
+            BarData barData = new BarData(barDataSet);
+
+            barChart.setData(barData);
+            barChart.invalidate();
         }
     }
 }
